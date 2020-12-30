3b82f96801a62dabfcfb018e8452648ce0c65af2
GuilleGorge213 <guillermogorgeaguire@gmail.com>
2020-12-30 22:55:42 +0100

    ERROR PLAnTING

--- juego/src/com/pochitoGames/Systems/People/LumberJackSystem.java
+++ juego/src/com/pochitoGames/Systems/People/LumberJackSystem.java
@@ -9,15 +9,18 @@ import com.pochitoGames.Components.People.LumberJack;
 import com.pochitoGames.Engine.Entity;
 import com.pochitoGames.Engine.System;
 import com.pochitoGames.Misc.Managers.TreeManager;
+import com.pochitoGames.Misc.Other.Vector2i;
+import com.pochitoGames.Misc.States.BuilderState;
 import com.pochitoGames.Misc.States.LumberJackState;
+import com.pochitoGames.Misc.States.WorkerState;
 
 import static com.pochitoGames.Misc.States.LumberJackState.CARRYING;
 import static com.pochitoGames.Misc.States.LumberJackState.CHOPPING;
 
 public class LumberJackSystem extends System {
 
-    public LumberJackSystem(){
-        include(Tree.class,Position.class, PathFinding.class, Human.class);
+    public LumberJackSystem() {
+        include(Tree.class, Position.class, PathFinding.class, Human.class);
         exclude(Builder.class);
     }
 
@@ -35,9 +38,9 @@ public class LumberJackSystem extends System {
 
                 case WALKING:
                     if (pf.getTargetCell() == null) {
-                        
-                    }
 
+                    }
+                    lumberJack.setLumberJackState(LumberJackState.CHOPPING);
                     break;
 
                 case CARRYING:
@@ -49,11 +52,27 @@ public class LumberJackSystem extends System {
                     break;
 
                 case PLANTING:
+                    Vector2i pantablePos = TreeManager.getInstance().getPlantableCell(pf.getCurrent());
+                    pf.setTargetCell(pantablePos);
+                    if (pf.getTargetCell() == null) {
+                    TreeManager.getInstance().createTree(pantablePos);
+
+                }
+                   //f (pf.getTargetCell() == null) {
+                   //   //Cojo al compa
+                   //   Builder mate = c.getTargetMate();
+                   //   //Cojo el edificio que está construyendo mi compa (getTargetBuilding)
+                   //   //Y le meto (putResources) una unidad del recurso que necesita (getResourcesNeeded)
+                   //   mate.getTargetBuilding().putResources(c.getCarrying(), 1);
+                   //   //Pongo al compa de vuelta al estado BUILDING (estaba en ON_HOLD)
+                   //   mate.setState(BuilderState.BUILD);
+                   //   //Me pongo en WAIT
+                   //   c.setState(WorkerState.WAIT);
 
                     break;
                 case SEARCH_TREE:
-                    Tree tree =TreeManager.getInstance().getNearestTree();
-                    if(tree != null){
+                    Tree tree = TreeManager.getInstance().getNearestTree();
+                    if (tree != null) {
                         pf.setTargetCell(tree.getCell());
                         lumberJack.setLumberJackState(LumberJackState.WALKING);
                         lumberJack.setTree(tree);
