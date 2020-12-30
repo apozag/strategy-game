71e0082c0ed2fb7580467d26d2341e7693d078d6
apozag <32688343+apozag@users.noreply.github.com>
2020-12-30 22:02:10 +0100

    cosas

---
+++ juego/src/com/pochitoGames/Systems/Buildings/WarehouseSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pochitoGames.Systems.Buildings;
+
+import com.pochitoGames.Engine.System;
+
+/**
+ *
+ * @author PochitoMan
+ */
+public class WarehouseSystem extends System{
+
+    public WarehouseSystem(){
+        
+    }
+    
+    @Override
+    public void update(double dt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
